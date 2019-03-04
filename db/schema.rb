2ee336ba2548a41cc0328a842287664509d683b2
aurelien <duquenneaurelien@gmail.com>
2019-03-04 17:47:18 +0100

    delete conflicts index and show

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,12 @@ ActiveRecord::Schema.define(version: 2019_03_04_153930) do
   enable_extension "plpgsql"
 
   create_table "carts", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "item_id"
+    t.bigint "user_id"
+    t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_carts_on_item_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -32,11 +34,13 @@ ActiveRecord::Schema.define(version: 2019_03_04_153930) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "item_id"
-    t.integer "user_id"
+    t.bigint "item_id"
+    t.bigint "user_id"
     t.string "stripe_customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
