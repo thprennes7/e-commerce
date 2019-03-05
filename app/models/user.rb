c52c5d0989319cc284e67bc9318a125bf08d4c34
aurelien <duquenneaurelien@gmail.com>
2019-03-05 15:01:42 +0100

    add file for Mailer Sendgrid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,25 @@
 class User < ApplicationRecord
+  after_create :welcome_send
   has_many :orders
   has_many :carts
   has_many :items, through: :order
 
   # validates
-#ybo : not possible for the moment
-#ybo  validates :first_name, presence: true
-#ybo  validates :last_name, presence: true
+  #ybo : not possible for the moment
+  #ybo  validates :first_name, presence: true
+  #ybo  validates :last_name, presence: true
   validates :email,
-    presence: true,
-    uniqueness: true,
-    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Email non valide" }
-  
+  presence: true,
+  uniqueness: true,
+  format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Email non valide" }
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
