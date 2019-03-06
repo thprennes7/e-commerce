822880aedc5482413138d5ec03361911c182f124
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-06 13:28:53 +0100

    cart backend

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,22 @@ class CartsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-  	
+  	@cart = Cart.where(user_id: current_user.id)
+
+  	@cart_price = 0
+  	@cart.each do |cart_item|
+  		@cart_price += cart_item.item.price
+  	end
+  end
+
+  def destroy
+  	cart_item = Cart.find_by(user_id: current_user.id, item_id: params[:id])
+
+  	if cart_item.destroy
+  		respond_to do |format|
+				format.html { redirect_to cart_path(current_user.id)}
+				format.js
+			end
+		end
   end
 end
