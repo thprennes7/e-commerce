b3102083715d874d0da2b390ba7fd9978e2b51ec
aurelien <duquenneaurelien@gmail.com>
2019-03-07 23:00:44 +0100

    wip spec

--- spec/controllers/carts_controller_spec.rb
+++ spec/controllers/carts_controller_spec.rb
@@ -2,18 +2,25 @@ require 'rails_helper'
 
 RSpec.describe CartsController, type: :controller do
   before(:all) do
-  @user  = FactoryBot.create(:user)
-  @item  = FactoryBot.create(:item)
-  @cart  = FactoryBot.create(:cart)
+    @user  = FactoryBot.create(:user)
+    @item  = FactoryBot.create(:item)
+    @cart  = FactoryBot.create(:cart)
   end
   describe "GET show" do
     it "expect carts" do
       get :show, params: {id: @cart}
-        expect(@cart).to eq(@cart)
+      expect(@cart).to eq(@cart)
     end
     it "respond_to carts" do
       get :show, params: {id: @cart}
-        expect(assigns(@cart)).respond_to?(:carts)
+      expect(assigns(@cart)).respond_to?(:carts)
     end
+  end
+  describe 'DELETE destroy' do
+    it "delete the cart" do
+      expect{
+        delete :destroy, current_user: {id: @user}, params: {id: @item}
+      }.to change(Cart, :count).by(-1)
+      end
   end
 end
