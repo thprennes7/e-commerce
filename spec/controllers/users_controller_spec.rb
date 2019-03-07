9d579bfff6ebf6bf557422da2f4a3af5f091f1a3
aurelien <duquenneaurelien@gmail.com>
2019-03-07 11:31:00 +0100

    wip spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  before(:all) do
+    @user = FactoryBot.create(:user)
+  describe "GET index" do
+    it "assigns @users" do
+      get :index
+      expect(assigns(:users)).to eq(user)
+    end
 
+    it "renders the index template" do
+      get :index
 end
