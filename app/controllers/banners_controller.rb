73b7f3d3de490473b898acf39b9b1d87ab4a34c5
Samkttn <kettani.samir@outlook.fr>
2019-03-07 22:03:16 +0100

    Banners comment

--- app/controllers/banners_controller.rb
+++ app/controllers/banners_controller.rb
@@ -6,7 +6,7 @@ class BannersController < ApplicationController
   def new
 
   end
-
+  #Method that saves the banner in different format once it is created.
   def create
     @banner = Banner.new(banner_params)
     respond_to do |format|
@@ -14,26 +14,28 @@ class BannersController < ApplicationController
         format.js
         format.html { redirect_to @banner, notice: 'Item was successfully created.' }
         format.json { render :show, status: :created, location: @banner }
+        #If the conditions are not met send an error without saving.
       else
         format.html { render :new }
         format.json { render json: @banner.errors, status: :unprocessable_entity }
       end
     end
   end
-
+  #Method that saves the banner in different format once it is modified.
   def update
     respond_to do |format|
       if @banner.update(title: params[:title], description: params[:description])
         format.js
         format.html { redirect_to @banner, notice: 'Item was successfully updated.' }
         format.json { render :show, status: :ok, location: @banner }
+        #If the conditions are not met send an error without saving.
       else
         format.html { render :edit }
         format.json { render json: @banner.errors, status: :unprocessable_entity }
       end
     end
   end
-
+#If the user is not admin, can not destroy the banner otherwise he can
   def destroy
     unless is_admin?(current_user)
       redirect_to request.referer
